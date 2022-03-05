c462924388fbb89c164eb899e0d7e2daf04a205e
Hez Ronningen <code@hez.ca>
2022-03-05 12:23:37 -0800

    use aliases for long module paths, making credo happy

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -15,6 +15,7 @@ defmodule HomeHub.DataCase do
   """
 
   use ExUnit.CaseTemplate
+  alias Ecto.Adapters.SQL
 
   using do
     quote do
@@ -28,8 +29,8 @@ defmodule HomeHub.DataCase do
   end
 
   setup tags do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(HomeHub.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = SQL.Sandbox.start_owner!(HomeHub.Repo, shared: not tags[:async])
+    on_exit(fn -> SQL.Sandbox.stop_owner(pid) end)
     :ok
   end
 
