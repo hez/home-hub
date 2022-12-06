fb5cbf5295903ceb82f6299afd48987e757ab9ee
Hez Ronningen <code@hez.ca>
2022-12-05 17:04:26 -0800

    deps and basic prod config

--- lib/home_hub/application.ex
+++ lib/home_hub/application.ex
@@ -7,22 +7,23 @@ defmodule HomeHub.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [
-      # Start the Telemetry supervisor
-      HomeHubWeb.Telemetry,
-      # Start the Ecto repository
-      HomeHub.Repo,
-      {HomeHub.Thermostat.Supervisor,
-       heater_io_config: [fan_pin: 17, heater_pin: 27, dht_pin: 18]},
-      # Start the PubSub system
-      {Phoenix.PubSub, name: HomeHub.PubSub},
-      # Start Finch
-      {Finch, name: HomeHub.Finch},
-      # Start the Endpoint (http/https)
-      HomeHubWeb.Endpoint
-      # Start a worker by calling: HomeHub.Worker.start_link(arg)
-      # {HomeHub.Worker, arg}
-    ]
+    children =
+      [
+        # Start the Telemetry supervisor
+        HomeHubWeb.Telemetry,
+        # Start the Ecto repository
+        HomeHub.Repo,
+        {HomeHub.Thermostat.Supervisor,
+         heater_io_config: [fan_pin: 17, heater_pin: 27, dht_pin: 18]},
+        # Start the PubSub system
+        {Phoenix.PubSub, name: HomeHub.PubSub},
+        # Start Finch
+        {Finch, name: HomeHub.Finch},
+        # Start the Endpoint (http/https)
+        HomeHubWeb.Endpoint
+        # Start a worker by calling: HomeHub.Worker.start_link(arg)
+        # {HomeHub.Worker, arg}
+      ] ++ prod_children()
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
@@ -36,5 +37,13 @@ defmodule HomeHub.Application do
   def config_change(changed, _new, removed) do
     HomeHubWeb.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  if Mix.env() == :prod do
+    def prod_children do
+      [{RpiScreenDimmer, []}]
+    end
+  else
+    def prod_children, do: []
   end
 end
