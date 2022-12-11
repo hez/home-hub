2eb085113b907de7c4f34932263f09d9848d783c
Hez Ronningen <code@hez.ca>
2022-12-11 13:58:10 -0800

    pull thermostat status display out of nav tab helper

--- lib/home_hub_web/components/layouts.ex
+++ lib/home_hub_web/components/layouts.ex
@@ -4,37 +4,41 @@ defmodule HomeHubWeb.Layouts do
   embed_templates "layouts/*"
 
   attr :active_tab, :atom
-  attr :status, :map, required: false
 
   def navigation_tabs(assigns) do
     ~H"""
-    <div>
-      <section class="container">
-        <nav role="navigation">
-          <ul class="flex">
-            <li class="flex-1 mr-2">
-              <.nav_link to={~p"/"} active={@active_tab == :dashboard}>Home</.nav_link>
-            </li>
-            <li class="flex-1 mr-2">
-              <.nav_link to={~p"/current"} active={@active_tab == :current}>Current</.nav_link>
-            </li>
-            <li class="flex-1 mr-2">
-              <.nav_link to={~p"/"} active={@active_tab == :history}>History</.nav_link>
-            </li>
-          </ul>
-        </nav>
-      </section>
-      <section class="container">
-        <%= if @status do %>
-          <div class="flex flex-row-reverse m-4">
-            <div class="text-4xl">
-              Current: <b><%= Float.round(@status.temperature, 1) %>&#176;C</b>
-              at <b><%= @status.humidity |> Float.round(0) |> trunc() %>%</b>
-            </div>
+    <section class="container">
+      <nav role="navigation">
+        <ul class="flex">
+          <li class="flex-1 mr-2">
+            <.nav_link to={~p"/"} active={@active_tab == :dashboard}>Home</.nav_link>
+          </li>
+          <li class="flex-1 mr-2">
+            <.nav_link to={~p"/current"} active={@active_tab == :current}>Current</.nav_link>
+          </li>
+          <li class="flex-1 mr-2">
+            <.nav_link to={~p"/"} active={@active_tab == :history}>History</.nav_link>
+          </li>
+        </ul>
+      </nav>
+    </section>
+    """
+  end
+
+  attr :status, :map, required: false
+
+  def thermostat_status(assigns) do
+    ~H"""
+    <section class="container">
+      <%= if @status do %>
+        <div class="flex flex-row-reverse m-4">
+          <div class="text-4xl">
+            Current: <b><%= Float.round(@status.temperature, 1) %>&#176;C</b>
+            at <b><%= @status.humidity |> Float.round(0) |> trunc() %>%</b>
           </div>
-        <% end %>
-      </section>
-    </div>
+        </div>
+      <% end %>
+    </section>
     """
   end
 
@@ -46,7 +50,7 @@ defmodule HomeHubWeb.Layouts do
     ~H"""
     <.link
       navigate={@to}
-      class={"text-center block rounded text-3xl py-2 px-4 text-blue-300 hover:border-gray-200 hover:bg-gray-200 #{if @active, do: "bg-black-100", else: "bg-gray-800 border-gray-800"}"}
+      class={"text-center block rounded text-3xl py-2 px-4 text-blue-300 #{if @active, do: "bg-black-100", else: "bg-gray-800 border-gray-800 hover:border-gray-200 hover:bg-gray-200"}"}
       aria-current={if @active, do: "true", else: "false"}
     >
       <%= render_slot(@inner_block) %>
