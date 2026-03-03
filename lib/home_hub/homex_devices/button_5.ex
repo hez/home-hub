0100e339c47947fbdec6c97813b1e2a8d8a6ccce
Hez Ronningen <code@hez.ca>
2026-03-02 18:23:39 -0800

    pass through false for storing of trigger value in entity

--- lib/home_hub/homex_devices/button_5.ex
+++ lib/home_hub/homex_devices/button_5.ex
@@ -1,5 +1,5 @@
 defmodule HomeHub.HomexDevice.Button5 do
   use Homex.Entity.DeviceTrigger, name: "button-5", subtype: "button_5"
 
-  def trigger, do: GenServer.cast(:button5, {:push_value, :trigger, "action"})
+  def trigger, do: GenServer.cast(:button5, {:push_value, :trigger, "action", false})
 end
--- lib/home_hub/homex_devices/button_6.ex
+++ lib/home_hub/homex_devices/button_6.ex
@@ -1,5 +1,5 @@
 defmodule HomeHub.HomexDevice.Button6 do
   use Homex.Entity.DeviceTrigger, name: "button-6", subtype: "button_6"
 
-  def trigger, do: GenServer.cast(:button6, {:push_value, :trigger, "action"})
+  def trigger, do: GenServer.cast(:button6, {:push_value, :trigger, "action", false})
 end
